9d192e40f3cc31b158ff91c8278c082eb6626ae2
Shireesha13 <133089515+Shireesha13@users.noreply.github.com>
2023-06-07 16:15:26 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,13 @@
+package javapractise;
+import java.lang.Math;
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println(" 1st random number :" +Math.random());     
+     System.out.println(" 2nd random number :" +Math.random());     
+     System.out.println(" 3rd random number :" +Math.random());     
+
+	}
+
+}
